c88d579d43fe2e3c41bbbceee7a9515bbf32bd59
xiaoyingbo <amazong-xyb@qq.com>
2022-09-29 08:26:43 +0800

    feat(lib_architecture): 更改lib命名

--- Lib-Framework-Demo/lib_framework/src/main/java/com/xiaoyingbo/lib_framework/data/response/networkState/NetworkStateCallback.java
+++
@@ -1,6 +0,0 @@
-package com.xiaoyingbo.lib_framework.data.response.networkState;
-
-/**网络状态变化回调*/
-public interface NetworkStateCallback {
-    default void networkStateChange(){}
-}
--- Lib-Framework-Demo/lib_framework/src/main/java/com/xiaoyingbo/lib_framework/ui/viewModel/Event.java
+++
@@ -1,8 +0,0 @@
-package com.xiaoyingbo.lib_framework.ui.viewModel;
-
-/**需要分发的事件*/
-public class Event<P, R> {
-    public int eventId;
-    public P param;
-    public R result;
-}
